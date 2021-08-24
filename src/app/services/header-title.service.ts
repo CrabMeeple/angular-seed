72efd457ec01b045831cd0d43e68fa6a4817f8c4
Tyler Yake <tyryake@gmail.com>
2021-08-24 09:26:45 -0500

    after first day

---
+++ src/app/services/header-title.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeaderTitleService {
+  getTitle(): string {
+    return 'Welcome to Angular Seed';
+  }
+  constructor() { }
+}
